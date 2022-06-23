b1dde40071f168f340022c30693c0fc519accb3b
Arun1102 <78301915+Arun1102@users.noreply.github.com>
2022-06-23 19:51:31 +0800

    Error fixed on Automapper

---
+++ StackOverflow.ServiceLayer/MapperExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.ServiceLayer
+{
+    public static class MapperExtensions
+    {
+        private static void IgnoreUnmappedProperties(TypeMap map, IMappingExpression expr)
+        {
+            foreach (string propName in map.GetUnmappedPropertyNames())
+            {
+                if (map.SourceType.GetProperty(propName) != null)
+                {
+                    expr.ForMember(propName, opt => opt.Ignore());
+                }
+                if (map.DestinationType.GetProperty(propName) != null)
+                {
+                    expr.ForMember(propName, opt => opt.Ignore());
+                }
+            }
+        }
+
+        public static void IgnoreUnmapped(this IProfileExpression profile)
+        {
+            profile.ForAllMaps(IgnoreUnmappedProperties);
+            
+        }
+    }
+
+}
